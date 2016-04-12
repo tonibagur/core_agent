98793358613c5570ad965a2980694aec82f6fd32
Toni Bagur <toni.bagur@gmail.com>
2016-04-12 11:25:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 setup(name='core_agent',
       version='0.1',
-      py_modules=['core_agent','notify_agent_state','script_call_function_cloud'],
+      py_modules=['core_agent','notify_agent_state','script_call_function_cloud','settings'],
       )
